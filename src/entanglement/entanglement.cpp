aba22a0da9a4a858fe01c2ac32a77cf149067b27
Yeonsu Jung <ynsuju@gmail.com>
2023-02-10 19:16:49 -0500

    struggling with positions

--- src/entanglement/entanglement.cpp
+++ src/entanglement/entanglement.cpp
@@ -17,11 +17,18 @@
 //
 // =============================================================================
 
+// TO DO: effects of compliance? - why it makes the rods sink
+
 #include "chrono/assets/ChTexture.h"
 #include "chrono/physics/ChBodyEasy.h"
 #include "chrono/physics/ChLinkMotorRotationSpeed.h"
 #include "chrono/physics/ChSystemNSC.h"
 #include "chrono_irrlicht/ChVisualSystemIrrlicht.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
 
 // Use the namespaces of Chrono
 using namespace chrono;
@@ -39,7 +46,7 @@ using namespace irr::gui;
 // Static values valid through the entire program (bad
 // programming practice, but enough for quick tests)
 
-float GLOBAL_friction = 0.3f;
+float GLOBAL_friction = 0.4f;
 float GLOBAL_cohesion = 0;
 float GLOBAL_compliance = 0;
 float GLOBAL_dampingf = 0.1f;
@@ -47,16 +54,16 @@ float GLOBAL_dampingf = 0.1f;
 // Some global variables used in this example
 // Consider using a class instead (see the other examples) <----- what does this mean?
 // TO DO: figure out effects of pre-factor
-// 
-int num_rods = 200;
+//
+int num_rods = 1000;
 double factor = 10;
-double rod_radius = 0.81/50*factor;
-double rod_length = 0.81*factor;
-double rod_density = 8000*factor;
+double rod_radius = 0.81 / 76 * factor*5;
+double rod_length = 0.81 * factor*5;
+double rod_density = 8000 * factor;
 
-double box_height = 10*factor;
-double box_width = 20*factor;
-double box_thickness = 1*factor;
+double box_height = 10 * factor;
+double box_width = 20 * factor;
+double box_thickness = 1 * factor;
 
 // Define a MyEventReceiver class which will be used to manage input
 // from the GUI graphical user interface
@@ -131,6 +138,75 @@ class MyEventReceiver : public IEventReceiver {
     IGUIStaticText* text_compliance;
 };
 
+void test_with_single_cylinder(ChSystemNSC& sys) {
+    auto mat = chrono_types::make_shared<ChMaterialSurfaceNSC>();
+    mat->SetFriction(0.4f);
+
+    auto cyl = chrono_types::make_shared<ChBodyEasyCylinder>(rod_radius, rod_length, rod_density, true, true, mat);
+    cyl->SetPos(ChVector<>(0, 0, 0));
+    cyl->SetRot(Q_from_AngAxis(CH_C_PI / 2, VECT_X));
+    sys.AddBody(cyl);
+}
+
+void load_rods_from_file(ChSystemNSC& sys) {
+    std::string file_path = "C:/Users/yjung/Dropbox (Harvard University)/Entangled/EnsembleAnalysis/Small_0/SmallRandomRods_[R1.2_H1.5_L1_A0.01_N1289_Date2023-02-08_12-56-38].csv";
+    auto mat = chrono_types::make_shared<ChMaterialSurfaceNSC>();
+    mat->SetFriction(0.4f);
+
+    std::ifstream myFile;
+    myFile.open(file_path);
+    int N = 0;
+    while (myFile.good()) {
+        std::string line;
+        std::getline(myFile, line, '\n');
+        N = N + 1;
+    }
+    GetLog() << "N: " << N << "\n";
+    myFile.close();
+
+    GetLog() << "Creating rods..." << "\n";
+    myFile.open(file_path);
+    double v[6]; // should it be something like a pointer?
+    int iter = 0;
+    while (myFile.good()) {
+        std::string line;
+        std::getline(myFile, line, '\n');
+        std::stringstream ss(line);
+
+        // if (line.empty() || iter > 10) {
+        if (iter > 50) {
+            break;
+        }
+        GetLog() << "iter: " << iter << "\n";
+        iter += 1;
+
+        for (int i = 0; i < 6; i++) {
+            // while (ss.good()) {
+            std::string substr;
+            std::getline(ss, substr, ',');
+            v[i] = std::stod(substr);
+        }
+
+        auto rod = chrono_types::make_shared<ChBodyEasyCylinder>(rod_radius, rod_length, rod_density, true, true, mat);
+        rod->SetPos(ChVector<>(0, 0, 0));
+        // rod->SetPos(ChVector<>((v[0] + v[3]) / 2 * factor*5, (v[1] + v[4]) / 2 * factor*5, (v[2] + v[5]) / 2 * factor*5));
+        rod->SetPos(ChVector<>((v[0]*factor*5,v[1]*factor*5,v[2]*factor*5)));
+        rod->GetVisualShape(0)->SetTexture(GetChronoDataFile("textures/rock.jpg"));
+
+        const ChVector<> v1(v[0], v[1], v[2]);
+        const ChVector<> v2(v[3], v[4], v[5]);
+
+        rod->SetRot(Q_from_Vect_to_Vect(v1, v2));
+        rod->SetEvalContactCn(true);
+        rod->SetEvalContactCt(true);
+        rod->SetEvalContactKf(true);
+        rod->SetEvalContactSf(true);
+        sys.Add(rod);
+
+        // GetLog() << v1[0] << '\n';
+    }
+}
+
 void create_some_falling_items(ChSystemNSC& sys) {
     // From now on, all created collision models will have a large outward envelope (needed
     // to allow some compliance with the plastic deformation of cohesive bounds
@@ -152,52 +228,60 @@ void create_some_falling_items(ChSystemNSC& sys) {
         // sys.Add(mrigidBody);
 
         // my change
-        auto mrigidBody = chrono_types::make_shared<ChBodyEasyCylinder>(rod_radius,  // radius
-                                                                        rod_length,       // height
-                                                                        rod_density,      // density
-                                                                        true,      // visualization?
-                                                                        true,      // collision?
-                                                                        obj_mat);  // contact material
-        mrigidBody->SetPos(ChVector<>(-5 + ChRandom() * 10 * factor, 4 + bi * 0.05  * factor, -5 + ChRandom() * 10 * factor));
+        auto mrigidBody = chrono_types::make_shared<ChBodyEasyCylinder>(rod_radius,   // radius
+                                                                        rod_length,   // height
+                                                                        rod_density,  // density
+                                                                        true,         // visualization?
+                                                                        true,         // collision?
+                                                                        obj_mat);     // contact material
+        mrigidBody->SetPos(
+            ChVector<>(-5 + ChRandom() * 10 * factor, 4 + bi * 0.05 * factor, -5 + ChRandom() * 10 * factor));
         mrigidBody->GetVisualShape(0)->SetTexture(GetChronoDataFile("textures/rock.jpg"));
         sys.Add(mrigidBody);
-    }
+    }    
+}
 
+void create_walls(ChSystemNSC& sys) {
     // Contact and visualization materials for container
     auto ground_mat = chrono_types::make_shared<ChMaterialSurfaceNSC>();
     auto ground_mat_vis = chrono_types::make_shared<ChVisualMaterial>(*ChVisualMaterial::Default());
     ground_mat_vis->SetKdTexture(GetChronoDataFile("textures/concrete.jpg"));
 
     // Create the five walls of the rectangular container, using fixed rigid bodies of 'box' type
-    auto floorBody = chrono_types::make_shared<ChBodyEasyBox>(box_width, box_thickness, box_width, 1000, true, true, ground_mat);
-    floorBody->SetPos(ChVector<>(0, -box_height/2, 0));
+    auto floorBody =
+        chrono_types::make_shared<ChBodyEasyBox>(box_width, box_thickness, box_width, 1000, true, true, ground_mat);
+    floorBody->SetPos(ChVector<>(0, -box_height / 2, 0));
     floorBody->SetBodyFixed(true);
     floorBody->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
     sys.Add(floorBody);
 
-    auto wallBody1 = chrono_types::make_shared<ChBodyEasyBox>(box_thickness, box_height, box_width+box_thickness - 0.01, 1000, true, true, ground_mat);
-    wallBody1->SetPos(ChVector<>(-box_width/2, 0, 0));
+    auto wallBody1 = chrono_types::make_shared<ChBodyEasyBox>(
+        box_thickness, box_height, box_width + box_thickness - 0.01, 1000, false, true, ground_mat);
+    wallBody1->SetPos(ChVector<>(-box_width / 2, 0, 0));
     wallBody1->SetBodyFixed(true);
-    wallBody1->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
+    // wallBody1->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
     sys.Add(wallBody1);
 
-    auto wallBody2 = chrono_types::make_shared<ChBodyEasyBox>(box_thickness, box_height, box_width+box_thickness - 0.01, 1000, false, true, ground_mat);
-    wallBody2->SetPos(ChVector<>(box_width/2, 0, 0));
+    auto wallBody2 = chrono_types::make_shared<ChBodyEasyBox>(
+        box_thickness, box_height, box_width + box_thickness - 0.01, 1000, false, true, ground_mat);
+    wallBody2->SetPos(ChVector<>(box_width / 2, 0, 0));
     wallBody2->SetBodyFixed(true);
     // wallBody2->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
     sys.Add(wallBody2);
 
-    auto wallBody3 = chrono_types::make_shared<ChBodyEasyBox>(box_width+box_thickness - 0.01, box_height, box_thickness, 1000, true, true, ground_mat);
-    wallBody3->SetPos(ChVector<>(0, 0, -box_width/2));
+    auto wallBody3 = chrono_types::make_shared<ChBodyEasyBox>(box_width + box_thickness - 0.01, box_height,
+                                                              box_thickness, 1000, false, true, ground_mat);
+    wallBody3->SetPos(ChVector<>(0, 0, -box_width / 2));
     wallBody3->SetBodyFixed(true);
-    wallBody3->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
+    // wallBody3->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
     sys.Add(wallBody3);
 
-    auto wallBody4 = chrono_types::make_shared<ChBodyEasyBox>(box_width+box_thickness - 0.01, box_height, box_thickness, 1000, true, true, ground_mat);
-    wallBody4->SetPos(ChVector<>(0, 0, box_width/2));
+    auto wallBody4 = chrono_types::make_shared<ChBodyEasyBox>(box_width + box_thickness - 0.01, box_height,
+                                                              box_thickness, 1000, false, true, ground_mat);
+    wallBody4->SetPos(ChVector<>(0, 0, box_width / 2));
     wallBody4->SetBodyFixed(true);
-    wallBody4->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
-    sys.Add(wallBody4);    
+    // wallBody4->GetVisualShape(0)->SetMaterial(0, ground_mat_vis);
+    sys.Add(wallBody4);
 }
 
 int main(int argc, char* argv[]) {
@@ -208,7 +292,10 @@ int main(int argc, char* argv[]) {
 
     // Create all the rigid bodies.
 
-    create_some_falling_items(sys);
+    // create_some_falling_items(sys);
+    create_walls(sys); // sys - as a reference
+    // test_with_single_cylinder(sys);
+    load_rods_from_file(sys);
 
     // Create the Irrlicht visualization system
     auto vis = chrono_types::make_shared<ChVisualSystemIrrlicht>();
@@ -216,9 +303,9 @@ int main(int argc, char* argv[]) {
     vis->SetWindowSize(800, 600);
     vis->SetWindowTitle("Contacts with cohesion");
     vis->Initialize();
-    vis->AddLogo();
+    // vis->AddLogo();
     vis->AddSkyBox();
-    vis->AddCamera(ChVector<>(0, 14*factor, -20*factor));
+    vis->AddCamera(ChVector<>(0, 5 * factor, -20 * factor));
     vis->AddTypicalLights();
 
     // This is for GUI tweaking of system parameters..
