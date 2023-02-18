19f9b2deccdb16e1e1164dc9aa43ef68e2d7bb18
Yeonsu Jung <ynsuju@gmail.com>
2023-02-18 01:25:07 -0500

    Update vapory_example.py

--- postproc/vapory_example.py
+++ postproc/vapory_example.py
@@ -1,7 +1,8 @@
-
 # %%
 import subprocess
-subprocess.call('dir', shell=True)
+# subprocess.call('dir', shell=True)
+subprocess.run(["svn","--version"], shell=True)
+
 from vapory import *
 
 camera = Camera( 'location', [0,2,-3], 'look_at', [0,1,2] )
@@ -10,3 +11,4 @@ sphere = Sphere( [0,1,2], 2, Texture( Pigment( 'color', [1,0,1] )))
 
 scene = Scene( camera, objects= [light, sphere])
 scene.render('ipython',width=400, height=300)
+# %%
